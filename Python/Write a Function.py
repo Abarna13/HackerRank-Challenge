95d001f47a12275befc30e55f3a2f099825aa3d5
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-12 19:03:34 +0530

    Create Write a Function.py

---
+++ Python/Write a Function.py
@@ -0,0 +1,19 @@
+def is_leap(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+    
+   
+    return leap
+
+year = int(input())
+print(is_leap(year))
